26e4604ab9442ae11c6f56c20e68e2e74189abb4
Rafael Fonseca <rafael.silvanofonseca@gmail.com>
2025-02-04 21:11:29 -0500

    return user data by id

--- app/database/models.py
+++ app/database/models.py
@@ -1,5 +1,5 @@
 import sqlite3
-from app.graphql.types import User
+from app.graphql.types import User, Scan
 
 def get_all_users():
     with sqlite3.connect('/db/hackers.db') as conn:
@@ -19,4 +19,46 @@ def get_all_users():
                 updated_at=row[4]
             )
             for row in c.fetchall()
-        ] 
+        ]
+
+def get_user_by_email(email):
+    with sqlite3.connect('/db/hackers.db') as conn:
+        c = conn.cursor()
+        c.execute('''
+            SELECT name, email, phone, badge_code, updated_at
+            FROM hackers
+            WHERE email = ?
+        ''', (email,))
+        
+        row = c.fetchone()
+        if row is None:
+            return None
+            
+        return User(
+            name=row[0],
+            email=row[1],
+            phone=row[2],
+            badge_code=row[3],
+            updated_at=row[4]
+        )
+
+def get_user_scans(email):
+    with sqlite3.connect('/db/hackers.db') as conn:
+        c = conn.cursor()
+        c.execute('''
+            SELECT a.activity_name, a.activity_category, s.scanned_at
+            FROM scans s
+            JOIN activities a ON s.activity_id = a.id
+            JOIN hackers h ON s.hacker_id = h.id
+            WHERE h.email = ?
+            ORDER BY s.scanned_at
+        ''', (email,))
+        
+        return [
+            Scan(
+                activity_name=row[0],
+                activity_category=row[1],
+                scanned_at=row[2]
+            )
+            for row in c.fetchall()
+        ]
--- app/graphql/schema.py
+++ app/graphql/schema.py
@@ -1,12 +1,16 @@
 import graphene
-from graphene import ObjectType, List
+from graphene import ObjectType, List, String
 from app.graphql.types import User
-from app.database.models import get_all_users
+from app.database.models import get_all_users, get_user_by_email
 
 class Query(ObjectType):
     users = List(User)
+    user = graphene.Field(User, email=String(required=True))
 
     def resolve_users(self, info):
         return get_all_users()
+    
+    def resolve_user(self, info, email):
+        return get_user_by_email(email)
 
 schema = graphene.Schema(query=Query) 
